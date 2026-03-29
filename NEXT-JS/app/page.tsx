f669b11776cbd0de3c1557770d946294cf237bb2
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2026-03-29 10:45:53 +0530

    fix: update layout and styling for dark mode support

--- NEXT-JS/app/page.tsx
+++ NEXT-JS/app/page.tsx
@@ -5,22 +5,19 @@ import { GradientBackgroundText } from "@/components/gradient-background-text";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center font-sans bg-background">
-      <GitHubStars repo="vercel/next.js" />
-      <h1 className="text-center text-4xl font-bold tracking-tighter md:text-5xl lg:text-6xl mb-8">
+      <GitHubStars repo="vercel/next.js" className="mb-8" />
+      <h1 className="text-center text-4xl font-bold tracking-tighter md:text-5xl lg:text-6xl max-w-[40%]">
         Welcome to{" "}
+        <GradientBackgroundText
+          colors="#ff6b6b, #ffd93d, #6bcb77, #4d96ff"
+          className="text-5xl lg:text-6xl"
+        >
+          Groot UI
+        </GradientBackgroundText>{" "} built with{" "}
         <GradientText colors="#cc0066, #1aff53, #004d99, #f5f56b, #a600e6">
           Next.js!
         </GradientText>
-        {" "}
-        <GradientBackgroundText
-        colors="#ff6b6b, #ffd93d, #6bcb77, #4d96ff"
-        className="text-5xl lg:text-6xl"
-      >
-        Groot UI
-      </GradientBackgroundText>
       </h1>
-
-      
     </div>
   );
 }
